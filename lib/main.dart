2f9261a2893e482b6934f366084955b4c7403b14
Marcelo Rodrigues Montalvão <marcelordrgs98@gmail.com>
2019-09-08 18:17:30 -0300

    resetando mensagem de erro

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class _HomeState extends State<Home> {
       weightController.text = "";
       heightController.text = "";
       _textInfo = "Informe seus dados!";
+      _formKey = GlobalKey<FormState>();
     });
   }
 
